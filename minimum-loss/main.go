c1d3c25ed09bfd58321e4dae67c2d199d7091e29
kyryl <truskovskiyk@gmail.com>
2018-04-22 19:52:45 +0300

    Minimum Loss  22.04.18

--- minimum-loss/main.go
+++ minimum-loss/main.go
@@ -11,72 +11,49 @@ import (
 
 type Item struct {
 	price int
-	year int
+	year  int
 }
-func readFromStdIn() (int, []int) {
+
+type ArrayOfItems []Item
+
+func (a ArrayOfItems) Len() int      { return len(a) }
+func (a ArrayOfItems) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ArrayOfItems) Less(i, j int) bool {
+	return a[i].price < a[j].price
+}
+
+func readFromStdIn() (int, []Item) {
 	return readTask(bufio.NewReader(os.Stdin))
 }
 
-
-func readFromFile(filePath string) (int, []int) {
+func readFromFile(filePath string) (int, []Item) {
 	f, _ := os.Open(filePath)
 	defer f.Close()
 	return readTask(bufio.NewReader(f))
 }
 
-
-func solveTaskFirst(numberDays int, prices []int) int{
-	//fmt.Println("numberDays", numberDays)
-	//fmt.Println("prices", prices)
-	minLoss := math.Inf(+1)
-	for i := int(0); i < numberDays; i++ {
-		for j := i + 1; j < numberDays; j++ {
-			b := prices[i]
-			c := prices[j]
-			loss := b - c
-			if c >= b {
-				continue
-			}
-			//fmt.Println("duy", i, "sell", j, "loss", loss)
-			//fmt.Println(c)
-			if float64(loss) < minLoss {
-				minLoss = float64(loss)
-			}
-		}
-	}
-	return int(minLoss)
-}
-
-func readTask(ioReader io.Reader) (int, []int) {
+func readTask(ioReader io.Reader) (int, []Item) {
 	inSource := bufio.NewReader(ioReader)
 
-	var n int
+	var n, temp int
 	fmt.Fscan(inSource, &n)
 
-	p := make([]int, n)
+	p := make([]Item, n)
 	for i := int(0); i < n; i++ {
-		fmt.Fscan(inSource, &p[i])
+		fmt.Fscan(inSource, &temp)
+		p[i] = Item{price: temp, year: i}
+
 	}
 	return n, p
 }
 
-func solveTaskSecond(numberDays int, prices []int) int{
-	items :=
-	sort.Ints(prices)
-	fmt.Println(prices)
-	//fmt.Println("numberDays", numberDays)
-	//fmt.Println("prices", prices)
+func solveTaskSecond(numberDays int, prices []Item) int {
+	sort.Sort(ArrayOfItems(prices))
 	minLoss := math.Inf(+1)
-	for i := int(0); i < numberDays; i++ {
-		for j := i + 1; j < numberDays; j++ {
-			b := prices[i]
-			c := prices[j]
-			loss := b - c
-			if c >= b {
-				continue
-			}
-			//fmt.Println("duy", i, "sell", j, "loss", loss)
-			//fmt.Println(c)
+	for i := int(0); i < numberDays-1; i++ {
+		if prices[i].year > prices[i+1].year {
+			//loss := prices[i].price - prices[i+1].price
+			loss := prices[i+1].price - prices[i].price
 			if float64(loss) < minLoss {
 				minLoss = float64(loss)
 			}
@@ -86,14 +63,11 @@ func solveTaskSecond(numberDays int, prices []int) int{
 }
 
 func main() {
-	fileName := "./DATA/in.txt"
-	n, p := readFromFile(fileName)
+	//fileName := "./DATA/in.txt"
+	//n, p := readFromFile(fileName)
 
-
-
-	//n, p := readFromStdIn()
+	n, p := readFromStdIn()
 	minLoss := solveTaskSecond(n, p)
 	fmt.Println(minLoss)
-
 
 }
