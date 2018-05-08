d6ff1976064d496235b0c17b3a7c5aeec4b31a35
kyryl <truskovskiyk@gmail.com>
2018-05-08 19:57:02 +0300

    start new challange

---
+++ breadth_first_search/test_main.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestBFS(t *testing.T) {
+	//total := FibonacciModified(10, 0, 1)
+	//if total != "84266613096281243382112" {
+	//	t.Errorf("FibonacciModified was incorrect, got: %v, want: %v.", total, "84266613096281243382112")
+	//}
+}
