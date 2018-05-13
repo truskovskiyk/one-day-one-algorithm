cc8cb31d75fd57032dfc5b2f617b96b01d2ad4ce
kyryl <truskovskiyk@gmail.com>
2018-05-14 00:23:28 +0300

    add read func

--- breadth_first_search/main.go
+++ breadth_first_search/main.go
@@ -1,7 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func main () {
-	fmt.Println("test")
+// Complete the bfs function below.
+func bfs(n int32, m int32, edges [][]int32, s int32) []int32 {
+	fmt.Println(n)
+	fmt.Println(m)
+	fmt.Println(edges)
+	fmt.Println(s)
+	return []int32{1, 2, 3}
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+	qTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	for qItr := 0; qItr < int(q); qItr++ {
+		nm := strings.Split(readLine(reader), " ")
+
+		nTemp, err := strconv.ParseInt(nm[0], 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		mTemp, err := strconv.ParseInt(nm[1], 10, 64)
+		checkError(err)
+		m := int32(mTemp)
+
+		var edges [][]int32
+		for i := 0; i < int(m); i++ {
+			edgesRowTemp := strings.Split(readLine(reader), " ")
+
+			var edgesRow []int32
+			for _, edgesRowItem := range edgesRowTemp {
+				edgesItemTemp, err := strconv.ParseInt(edgesRowItem, 10, 64)
+				checkError(err)
+				edgesItem := int32(edgesItemTemp)
+				edgesRow = append(edgesRow, edgesItem)
+			}
+
+			if len(edgesRow) != int(2) {
+				panic("Bad input")
+			}
+
+			edges = append(edges, edgesRow)
+		}
+
+		sTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+		s := int32(sTemp)
+
+		result := bfs(n, m, edges, s)
+
+		for i, resultItem := range result {
+			fmt.Fprintf(writer, "%d", resultItem)
+
+			if i != len(result) - 1 {
+				fmt.Fprintf(writer, " ")
+			}
+		}
+
+		fmt.Fprintf(writer, "\n")
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
