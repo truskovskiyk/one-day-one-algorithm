c74ef5a139a07030be35a0b9711e1fb8c9620097
kyryl <truskovskiyk@gmail.com>
2018-10-07 22:52:31 +0300

    refactor done

--- gridland-metro/main.go
+++ gridland-metro/main.go
@@ -10,60 +10,105 @@ import (
 	"sort"
 )
 
+type Interval struct {
+	start int
+	end   int
+}
+
+func isOverlap(u Interval, v Interval) bool {
+	if u.start < v.start {
+		if u.end < v.start {
+			return false
+		}
+	} else if v.start < u.start {
+		if v.end < u.start {
+			return false
+		}
+	}
+	return true
+}
+
+func findOneLineTrackLength(oneLineTrack []Interval) int {
+	sort.Slice(oneLineTrack, func(i, j int) bool { return oneLineTrack[i].start < oneLineTrack[j].start })
+
+	stack := make([]Interval, 0)
+	stack = append(stack, oneLineTrack[0])
+
+	for i := 0; i < len(oneLineTrack); i++ {
+		stackTop := stack[len(stack)-1]
+		if isOverlap(stackTop, oneLineTrack[i]) {
+			if stackTop.end < oneLineTrack[i].end {
+				stackTop.end = oneLineTrack[i].end
+				stack[len(stack)-1] = stackTop
+			}
+		} else {
+			stack = append(stack, oneLineTrack[i])
+
+		}
+	}
+	totalLength := 0
+	for i := 0; i < len(stack); i++ {
+		totalLength += stack[i].end - stack[i].start + 1
+	}
+	return totalLength
+}
 
 // Complete the gridlandMetro function below.
-func GridlandMetro(n int32, m int32, k int32, tracks [][]int32) int32 {
-
-	sort.Slice(tracks, func(i, j int) bool { return tracks[i][0] < tracks[j][0] })
-
-	counter := make(map[int32][][]int32)
+func GridlandMetro(n int, m int, k int, tracks [][]int) int {
+	mapOfLineToTracks := make(map[int][]Interval)
 	for i := 0; i < len(tracks); i++ {
-
 		track := tracks[i]
-		fmt.Println(track)
 		r := track[0]
 		c1 := track[1]
 		c2 := track[2]
-		counter[r] = append(counter[r], []int32{c1, c2})
+
+		interval := Interval{c1, c2}
+		mapOfLineToTracks[r] = append(mapOfLineToTracks[r], interval)
 	}
-	fmt.Println(counter)
-	return int32(1)
+
+	l := 0
+	for _, oneLineTrack := range mapOfLineToTracks {
+		l += findOneLineTrackLength(oneLineTrack)
+
+	}
+	r := n*m - l
+	return r
 
 }
 
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
 	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
 	checkError(err)
 
 	defer stdout.Close()
 
-	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
 
 	nmk := strings.Split(readLine(reader), " ")
 
 	nTemp, err := strconv.ParseInt(nmk[0], 10, 64)
 	checkError(err)
-	n := int32(nTemp)
+	n := int(nTemp)
 
 	mTemp, err := strconv.ParseInt(nmk[1], 10, 64)
 	checkError(err)
-	m := int32(mTemp)
+	m := int(mTemp)
 
 	kTemp, err := strconv.ParseInt(nmk[2], 10, 64)
 	checkError(err)
-	k := int32(kTemp)
+	k := int(kTemp)
 
-	var track [][]int32
+	var track [][]int
 	for i := 0; i < int(k); i++ {
 		trackRowTemp := strings.Split(readLine(reader), " ")
 
-		var trackRow []int32
+		var trackRow []int
 		for _, trackRowItem := range trackRowTemp {
 			trackItemTemp, err := strconv.ParseInt(trackRowItem, 10, 64)
 			checkError(err)
-			trackItem := int32(trackItemTemp)
+			trackItem := int(trackItemTemp)
 			trackRow = append(trackRow, trackItem)
 		}
 
--- gridland-metro/main_test.go
+++ gridland-metro/main_test.go
@@ -2,36 +2,34 @@ package main
 
 import "testing"
 
-//func TestGameOfThronesCase1(t *testing.T) {
-//	n := int32(4)
-//	m := int32(4)
-//	k := int32(3)
-//
-//	track := [][]int32{{2, 2, 3}, {3, 1, 4}, {4, 4, 4}}
-//	result := GridlandMetro(n, m, k, track)
-//	answer := int32(9)
-//	if result != answer {
-//		t.Errorf("TestGameOfThronesCase1 was incorrect, got: %v, want: %v.", result, answer)
-//	}
-//
-//}
-
-
 func TestGameOfThronesCase2(t *testing.T) {
-	n := int32(1)
-	m := int32(8)
-	k := int32(1)
+	n := 4
+	m := 4
+	k := 3
 
-	track := [][]int32{
-		{1, 2, 3},
-		{1, 3, 6},
-		{1, 2, 7},
-
-		}
+	track := [][]int{{2, 2, 3}, {3, 1, 4}, {4, 4, 4}}
 	result := GridlandMetro(n, m, k, track)
-	answer := int32(1)
+	answer := 9
 	if result != answer {
 		t.Errorf("TestGameOfThronesCase1 was incorrect, got: %v, want: %v.", result, answer)
+	}
+
+}
+
+func TestGridlandMetroCase1(t *testing.T) {
+	n := 2
+	m := 8
+	k := 1
+
+	track := [][]int{
+		{2, 2, 3},
+		{1, 3, 6},
+		{1, 2, 7},
+	}
+	result := GridlandMetro(n, m, k, track)
+	answer := 8
+	if result != answer {
+		t.Errorf("TestGridlandMetroCase1 was incorrect, got: %v, want: %v.", result, answer)
 	}
 
 }
