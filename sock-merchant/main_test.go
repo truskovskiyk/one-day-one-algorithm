001971172dccd2ff1f0b1a48fac9a274678e71d0
kyryl <truskovskiyk@gmail.com>
2018-12-23 21:59:12 +0200

    done

--- sock-merchant/main_test.go
+++ sock-merchant/main_test.go
@@ -2,10 +2,12 @@ package main
 
 import "testing"
 
-func testSmoke(t *testing.T) {
-	result := 1
-	answer := 1
+func TestSockMerchant(t *testing.T) {
+	n := 9
+	arr := []int{10, 20, 20, 10, 10, 30, 50, 10, 20}
+	answer := 3
+	result := sockMerchant(n, arr)
 	if result != answer {
-		t.Errorf("gameOfThrones was incorrect, got: %v, want: %v.", result, answer)
+		t.Errorf("sockMerchant was incorrect, got: %v, want: %v.", result, answer)
 	}
 }
