326cc7dabb6de858f4da5c01ec29424a3849ae0a
Kyryl Truskovskyi <truskovskiyk@gmail.com>
2018-12-25 01:55:00 +0200

    JumpingOnClouds (#12)

---
+++ jumping-on-the-clouds/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the jumpingOnClouds function below.
+func jumpingOnClouds(c []int) int {
+	if len(c) == 1 {
+		return 0
+	}
+	if len(c) == 2 {
+		return 1
+	}
+	numJumps:= 0
+	for i := 0; i < len(c) - 1;  {
+		if (i + 2) < len(c) && c[i + 2] == 0 {
+			i += 2
+		} else {
+			i += 1
+		}
+		numJumps += 1
+	}
+	return numJumps
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int(nTemp)
+
+	cTemp := strings.Split(readLine(reader), " ")
+
+	var c []int
+
+	for i := 0; i < int(n); i++ {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int(cItemTemp)
+		c = append(c, cItem)
+	}
+
+	result := jumpingOnClouds(c)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ jumping-on-the-clouds/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+
+func TestJumpingOnCloudsOneCloud(t *testing.T) {
+	answer := 0
+	clouds := []int{0}
+	result := jumpingOnClouds(clouds)
+	if result != answer {
+		t.Errorf("jumpingOnClouds was incorrect, got: %v, want: %v.", result, answer)
+	}
+}
+
+
+func TestJumpingOnCloudsTwoClouds(t *testing.T) {
+	answer := 1
+	clouds := []int{0, 0}
+	result := jumpingOnClouds(clouds)
+	if result != answer {
+		t.Errorf("jumpingOnClouds was incorrect, got: %v, want: %v.", result, answer)
+	}
+}
+
+
+func TestJumpingOnCloudsCase1(t *testing.T) {
+	answer := 4
+	clouds := []int{0, 0, 1, 0, 0, 1, 0}
+	result := jumpingOnClouds(clouds)
+	if result != answer {
+		t.Errorf("jumpingOnClouds was incorrect, got: %v, want: %v.", result, answer)
+	}
+}
+
+
+func TestJumpingOnCloudsCase2(t *testing.T) {
+	answer := 3
+	clouds := []int{0, 0, 0, 1, 0, 0}
+	result := jumpingOnClouds(clouds)
+	if result != answer {
+		t.Errorf("jumpingOnClouds was incorrect, got: %v, want: %v.", result, answer)
+	}
+}
