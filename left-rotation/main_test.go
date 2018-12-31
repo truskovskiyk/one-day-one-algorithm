a9e4d4e4bf32fe4ed0cfb3b29b446cf82144eece
kyryl <truskovskiyk@gmail.com>
2018-12-31 22:42:09 +0200

    tests

--- left-rotation/main_test.go
+++ left-rotation/main_test.go
@@ -2,13 +2,26 @@ package main
 
 import "testing"
 
+func isIntArrayEquals(a []int, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+
 func TestRotLeft(t *testing.T) {
 
-	n := 8
-	path := "UDDDUDUU"
-	answer := 1
-	result := countingValleys(n, path)
-	if result != answer {
+	d := 4
+	arr := []int{1, 2, 3, 4, 5}
+	answer := []int{5, 1, 2, 3, 4}
+	result := rotLeft(arr, d)
+	if !isIntArrayEquals(result, answer) {
 		t.Errorf("rotLeft was incorrect, got: %v, want: %v.", result, answer)
 	}
 }
