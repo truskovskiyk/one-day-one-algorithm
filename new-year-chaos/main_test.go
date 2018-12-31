1d6fdd3c64ac88db4e52093964852724fdbd64cf
kyryl <truskovskiyk@gmail.com>
2018-12-31 23:22:13 +0200

    New-Year-Chaos

---
+++ new-year-chaos/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+
+func TestminimumBribes(t *testing.T) {
+	result := minimumBribes(arr, d)
+	if result != answer) {
+		t.Errorf("minimumBribes was incorrect, got: %v, want: %v.", result, answer)
+	}
+}
